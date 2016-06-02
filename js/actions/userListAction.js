c1463ff456b75619378d705e16866f70f2e1a2fa
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-06-02 18:01:50 +0800

    [#3] - Changed to use redux-thunk middleware to handle promise.

--- js/actions/userListAction.js
+++ js/actions/userListAction.js
@@ -1,16 +1,18 @@
 export const FETCH_USER = 'FETCH_USER'
 
 export function userListAction() {
-  return fetch('http://facehub.net/api/users', { headers: { Cookie: 'token=KssYGK1SwjRuX26AMQAubJ' } })
+  return dispatch => fetch('http://facehub.net/api/users', { headers: { Cookie: 'token=KssYGK1SwjRuX26AMQAubJ' } })
     .then(res => {
       if (res.ok) {
         return res.json()
       }
       throw res.error()
     })
-    .then(json => ({
-      type: FETCH_USER,
-      payload: json.users
-    }))
+    .then(json => {
+      dispatch({
+        type: FETCH_USER,
+        payload: json.users
+      })
+    })
     .catch(error => console.info('request error: ', error))
 }
--- js/store/index.js
+++ js/store/index.js
@@ -1,13 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
 import { userListReducer } from '../reducers/userListReducer'
 
-const promiseMiddleware = () => (next) => (action) => {
-  if (typeof action.then === 'function') {
-    return Promise.resolve(action).then(next)
-  }
-  return next(action)
-}
-
 export default createStore(userListReducer, compose(
-  applyMiddleware(promiseMiddleware)
+  applyMiddleware(thunk)
 ))
