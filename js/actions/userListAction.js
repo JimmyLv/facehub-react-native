6e3dc69b4ffef6b7508edd88a3d524f535f8fdaa
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-06-02 17:52:42 +0800

    [#3] - Implement self promise middleware for redux.

--- js/actions/userListAction.js
+++ js/actions/userListAction.js
@@ -1,35 +1,16 @@
-const data = [
-  {
-    name: ' JingLv',
-    office: '武汉',
-    title: 'HR',
-    photo: 'http://nobackend.website/_ng/images/avatar.jpg',
-    created_at: '2016-03-04 15:38:48',
-    id: 151,
-    project: 'Wuhan HR',
-    avatar: 'http://nobackend.website/_ng/images/avatar.jpg',
-    onboard: '2014-12-04',
-    email: 'jinglv@gmail.com'
-  },
-  {
-    name: 'JimmyLv',
-    office: '成都',
-    title: 'Dev',
-    photo: 'http://nobackend.website/_ng/images/avatar.jpg',
-    created_at: '2016-03-04 15:57:00',
-    id: 152,
-    project: 'Nobackend',
-    avatar: 'http://nobackend.website/_ng/images/avatar.jpg',
-    onboard: '2015-07-20',
-    email: 'jimmy.jinglv@gmail.com'
-  }
-]
-
 export const FETCH_USER = 'FETCH_USER'
 
 export function userListAction() {
-  return {
-    type: FETCH_USER,
-    payload: data
-  }
+  return fetch('http://facehub.net/api/users', { headers: { Cookie: 'token=KssYGK1SwjRuX26AMQAubJ' } })
+    .then(res => {
+      if (res.ok) {
+        return res.json()
+      }
+      throw res.error()
+    })
+    .then(json => ({
+      type: FETCH_USER,
+      payload: json.users
+    }))
+    .catch(error => console.info('request error: ', error))
 }
